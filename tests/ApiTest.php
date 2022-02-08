98b3d3c5c3a9ff95d7fc4167ac6fc793adb2bd82
Patrizio Bekerle <patrizio@bekerle.com>
2022-02-08 16:59:50 +0100

    Add basic tests

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -9,32 +9,33 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ApiTest extends ApiTestCase
 {
-    public function testBasics()
-    {
-        $client = self::createClient();
-        $response = $client->request('GET', '/forms/formdatas');
-        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
-
-        $response = $client->request('GET', '/forms/formdatas/graz');
-        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
-
-        $response = $client->request('DELETE', '/forms/formdatas/graz');
-        $this->assertSame(Response::HTTP_NO_CONTENT, $response->getStatusCode());
-
-        $response = $client->request('PUT', '/forms/formdatas/graz', [
-            'headers' => [
-                'Content-Type' => 'application/json',
-            ],
-            'body' => json_encode(['name' => 'foo']),
-        ]);
-        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
-        $this->assertSame('foo', json_decode($response->getContent(), true)['name']);
-    }
+//    public function testBasics()
+//    {
+//        $client = self::createClient();
+//        $response = $client->request('POST', '/forms/form_datas', [
+//            'headers' => [
+//                'Content-Type' => 'application/json',
+//            ],
+//            'body' => json_encode(['data' => 'foo']),
+//        ]);
+//        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
+//        $this->assertSame('foo', json_decode($response->getContent(), true)['data']);
+//    }
 
     public function testNoAuth()
     {
-        $client = self::createClient();
-        $response = $client->request('GET', '/forms/formdatas/graz/loggedin-only');
-        $this->assertSame(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+        $endpoints = [
+            ['POST', '/forms/form_datas', 401],
+            ['GET', '/forms/form_datas', 401],
+            ['GET', '/forms/form_datas/123', 404],
+        ];
+
+        foreach ($endpoints as $ep) {
+            [$method, $path, $status] = $ep;
+            $client = self::createClient();
+            $response = $client->request($method, $path);
+            $this->assertEquals($status, $response->getStatusCode(), $path);
+        }
+
     }
 }
