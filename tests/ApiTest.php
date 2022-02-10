8d638aa0179336bb74de1d3f7de18fa67167076c
Patrizio Bekerle <patrizio@bekerle.com>
2022-02-10 17:17:49 +0100

    Fix tests

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -9,23 +9,22 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ApiTest extends ApiTestCase
 {
-//    public function testBasics()
-//    {
-//        $client = self::createClient();
-//        $response = $client->request('POST', '/formalize/submissions', [
-//            'headers' => [
-//                'Content-Type' => 'application/json',
-//            ],
-//            'body' => json_encode(['data' => 'foo']),
-//        ]);
-//        $this->assertSame(Response::HTTP_OK, $response->getStatusCode());
-//        $this->assertSame('foo', json_decode($response->getContent(), true)['data']);
-//    }
+    public function testBasics()
+    {
+        $client = self::createClient();
+        $response = $client->request('POST', '/formalize/submissions', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'body' => json_encode(['dataFeedElement' => 'foo']),
+        ]);
+        $this->assertSame(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertSame('foo', json_decode($response->getContent(), true)['dataFeedElement']);
+    }
 
     public function testNoAuth()
     {
         $endpoints = [
-            ['POST', '/formalize/submissions', 401],
             ['GET', '/formalize/submissions', 401],
             ['GET', '/formalize/submissions/123', 404],
         ];
@@ -34,7 +33,6 @@ class ApiTest extends ApiTestCase
             [$method, $path, $status] = $ep;
             $client = self::createClient();
             $response = $client->request($method, $path);
-            var_dump($response);
             $this->assertEquals($status, $response->getStatusCode(), $path);
         }
 
