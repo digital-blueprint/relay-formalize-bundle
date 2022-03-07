e51b11e313a86298bf69a51d6415750487518273
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2022-03-07 16:01:45 +0100

    Fix linting problem

--- src/Migrations/Version20220307144853.php
+++ src/Migrations/Version20220307144853.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Rename formalize_submissions
+ * Rename formalize_submissions.
  */
 final class Version20220307144853 extends AbstractMigration
 {
