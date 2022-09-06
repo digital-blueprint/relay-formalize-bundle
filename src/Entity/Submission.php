d566fd201905d3a91c090a378fb5cacc67a00ff4
Tamara Steinwender <tamara.steinwender@tugraz.at>
2022-09-06 14:03:13 +0200

    Change code styling

--- src/Entity/Submission.php
+++ src/Entity/Submission.php
@@ -128,7 +128,7 @@ class Submission
         
         // If there is a diff between old and new scheme throw an error
         if (!empty($diffKey)) {
-            throw ApiError::withDetails(Response::HTTP_UNPROCESSABLE_ENTITY, 'The dataFeedElement doesn\'t match with the pevious submissions of the form: \''.$formName.'\' (the keys must correspond to scheme: \''.implode( "', '", array_keys($dataFeedElementPrev)).'\')', 'formalize:submission-invalid-json-keys');
+            throw ApiError::withDetails(Response::HTTP_UNPROCESSABLE_ENTITY, 'The dataFeedElement doesn\'t match with the pevious submissions of the form: \''.$formName.'\' (the keys must correspond to scheme: \''.implode("', '", array_keys($dataFeedElementPrev)).'\')', 'formalize:submission-invalid-json-keys');
         }
     }
 
