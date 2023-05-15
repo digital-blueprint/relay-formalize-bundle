ff10d5ba36f133e3bcd56bd61b3c4fa49dc59fda
Christoph Reiter <reiter.christoph@gmail.com>
2023-05-15 11:22:12 +0200

    Update to api-platform 2.7

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -54,5 +54,9 @@ class Kernel extends BaseKernel
         $container->extension('dbp_relay_formalize', [
             'database_url' => 'mysql://dummy:dummy@dummy',
         ]);
+
+        $container->extension('api_platform', [
+            'metadata_backward_compatibility_layer' => false,
+        ]);
     }
 }
