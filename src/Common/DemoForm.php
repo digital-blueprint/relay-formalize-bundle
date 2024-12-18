526c65ca9765e4b9bd89b61ee8960287683ea4a4
Patrizio Bekerle <patrizio@bekerle.com>
2024-12-18 15:13:15 +0100

    migration: add demo form
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

---
+++ src/Common/DemoForm.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\FormalizeBundle\Common;
+
+class DemoForm
+{
+    public const REGISTRATIONS_FORM_IDENTIFIER = 'e78869ce-e9b3-4df2-854b-cf88a35285f5';
+}
---
+++ src/Migrations/Version20241218144500.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\FormalizeBundle\Migrations;
+
+use Dbp\Relay\FormalizeBundle\Common\DemoForm;
+use Doctrine\DBAL\Schema\Schema;
+
+final class Version20241218144500 extends EntityManagerMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create the demo form.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('INSERT INTO formalize_forms (identifier, name, date_created) VALUES ("'.DemoForm::REGISTRATIONS_FORM_IDENTIFIER.'", "Demo Form", CURRENT_TIMESTAMP)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DELETE FROM formalize_forms WHERE identifier = "'.DemoForm::REGISTRATIONS_FORM_IDENTIFIER.'"');
+    }
+}
