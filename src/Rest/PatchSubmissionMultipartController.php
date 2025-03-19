311964a74fefc2bec59b3198fb59eceb6b5729a7
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2025-03-19 10:21:45 +0100

    update core

--- src/Rest/PatchSubmissionMultipartController.php
+++ src/Rest/PatchSubmissionMultipartController.php
@@ -5,11 +5,8 @@ declare(strict_types=1);
 namespace Dbp\Relay\FormalizeBundle\Rest;
 
 use Dbp\Relay\CoreBundle\Exception\ApiError;
+use Dbp\Relay\CoreBundle\Rest\Tools;
 use Dbp\Relay\FormalizeBundle\Entity\Submission;
-use Kekos\MultipartFormDataParser\Parser;
-use Nyholm\Psr7\Factory\Psr17Factory;
-use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
-use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 use Symfony\Component\HttpFoundation\Request;
 
 class PatchSubmissionMultipartController extends AbstractSubmissionMultipartController
@@ -23,7 +20,7 @@ class PatchSubmissionMultipartController extends AbstractSubmissionMultipartCont
     {
         $this->requireAuthentication();
 
-        $request = self::convertPatchRequest($request);
+        $request = Tools::fixMultipartPatchRequest($request);
 
         $submission = $this->formalizeService->getSubmissionByIdentifier($identifier);
         $previousSubmission = clone $submission;
@@ -37,24 +34,5 @@ class PatchSubmissionMultipartController extends AbstractSubmissionMultipartCont
         $this->submittedFileService->removeSubmittedFilesFromSubmission($submittedFilesToDelete, $submission);
 
         return $this->formalizeService->updateSubmission($submission, $previousSubmission);
-    }
-
-    private static function convertPatchRequest(Request $request): Request
-    {
-        $uploaded_file_factory = new Psr17Factory();
-        $stream_factory = $uploaded_file_factory;
-
-        $bridgeFactory = new Psr17Factory();
-        $psrHttpFactory = new PsrHttpFactory($bridgeFactory, $bridgeFactory, $bridgeFactory, $bridgeFactory);
-
-        // convert to psr7 to decorate request with multipart formdata
-        $psrRequest = $psrHttpFactory->createRequest($request);
-        $parser = Parser::createFromRequest($psrRequest, $uploaded_file_factory, $stream_factory);
-        $psrRequest = $parser->decorateRequest($psrRequest);
-
-        // convert back to symfony request to get a symfony uploadedFile instead of a PSR-7 uploadedFile
-        $httpFoundationFactory = new HttpFoundationFactory();
-
-        return $httpFoundationFactory->createRequest($psrRequest);
     }
 }
